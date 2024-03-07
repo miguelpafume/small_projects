c064099b3df33ef39385f5c5d96ea44954be7d46
miguelpafume <leugim.pafume@gmail.com>
2024-03-07 18:44:28 -0300

    cleaned up the code a bit (unfinished, but still works)

--- NumberConverter/NumberConverter.cpp
+++ NumberConverter/NumberConverter.cpp
@@ -1,3 +1,8 @@
+/*
+THIS PROGRAMS IS WAS MADE TO HELP SOLVE A HOMEWORK ASSIGNMENT
+THE CONVERSION METHODS USED ARE PROBLABLY INEFICIENT AS IT IS DONE THROUGH THE METHODS ASKED BY MY INSTRUCTOR
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -5,16 +10,8 @@
 
 using namespace std;
 
-int CountDigits(int x) {
-  int i = 0;
-  while(x > 0) {
-    x /= 10;
-    i++;
-  }
-  return i;
-}
-
-int BinaryToOct(int x) {
+//Translates from binary to oct
+int BinaryToOct(const int &x) {
   switch(x) {
     case 0: return 0;
     case 1: return 1;
@@ -27,7 +24,8 @@ int BinaryToOct(int x) {
   }
 }
 
-char BinaryToHex(int x) {
+//Translates from binary to hex
+char BinaryToHex(const int &x) {
   switch(x) {
     case 0: return '0';
     case 1: return '1';
@@ -48,38 +46,65 @@ char BinaryToHex(int x) {
   }
 }
 
-int HexTranslate(char x) {
+//Translates from oct to binary
+vector<int> OctTranslate(const int x) {
   switch(x) {
-    case '0': return 0;
-    case '1': return 1;
-    case '2': return 10;
-    case '3': return 11;
-    case '4': return 100;
-    case '5': return 101;
-    case '6': return 110;
-    case '7': return 111;
-    case '8': return 1000;
-    case '9': return 1001;
-    case 'A': return 1010;
-    case 'B': return 1011;
-    case 'C': return 1100;
-    case 'D': return 1101;
-    case 'E': return 1110;
-    default: return 1111;
+    case 0:   return {0, 0, 0};
+    case 1:   return {0, 0, 1};
+    case 2:   return {0, 1, 0};
+    case 3:   return {0, 1, 1};
+    case 4:   return {1, 0, 0};
+    case 5:   return {1, 0, 1};
+    case 6:   return {1, 1, 0};
+    default:  return {1, 1, 1};
   }
 }
 
-void IntToVector(vector<int> *vec, long long unsigned int x) {
+//Translates from hex to binary
+vector<int> HexTranslate(const char x) {
+  switch(x) {
+    case '0': return {0, 0, 0, 0};
+    case '1': return {0, 0, 0, 1};
+    case '2': return {0, 0, 1, 0};
+    case '3': return {0, 0, 1, 1};
+    case '4': return {0, 1, 0, 0};
+    case '5': return {0, 1, 0, 1};
+    case '6': return {0, 1, 1, 0};
+    case '7': return {0, 1, 1, 1};
+    case '8': return {1, 0, 0, 0};
+    case '9': return {1, 0, 0, 1};
+    case 'A': return {1, 0, 1, 0};
+    case 'B': return {1, 0, 1, 1};
+    case 'C': return {1, 1, 0, 0};
+    case 'D': return {1, 1, 0, 1};
+    case 'E': return {1, 1, 1, 0};
+    default:  return {1, 1, 1, 1};
+  }
+}
+
+//Returns a vector based on the input integer
+vector<int> IntToVector(long long unsigned int x) {
+  vector<int> int_vec {};
   while(x > 0) {
-    int num {static_cast<int>((x)%10)};
-    x /= 10;
-    (*vec).insert((*vec).begin(), num);
+    long long unsigned int num {x%10}; // num = last digit
+    x /= 10; //Removes the last digit
+    int_vec.insert(int_vec.begin(), num);
   }
+  return int_vec;
 }
 
-void DecimalToBinary(vector<int> *vec, unsigned int decimal) {
-  cout << endl;
+//Returns a decimal value (size_t) with based on the binary input
+size_t BinaryToDecimal(const vector<int>* const vec) {
+  size_t decimal_value {0};
+  for(int i {static_cast<int>((*vec).size())-1}; i >= 0; i--) {
+    if((*vec).at((*vec).size()-i-1) == 1) {
+      decimal_value += pow(2, i);
+    }
+  }
+  return decimal_value;
+}
 
+void DecimalToBinary(vector<int>* vec, unsigned int decimal) {
   while(decimal!=0) {
     int temp {0};
     temp = decimal % 2;
@@ -89,39 +114,16 @@ void DecimalToBinary(vector<int> *vec, unsigned int decimal) {
   }
 }
 
-int OctTranslate(int x) {
-  switch(x) {
-    case 0: return 0;
-    case 1: return 1;
-    case 2: return 10;
-    case 3: return 11;
-    case 4: return 100;
-    case 5: return 101;
-    case 6: return 110;
-    default: return 111;
-  }
-}
-
-void OctToBinary(vector<int> *vec) {
+void OctToBinary(vector<int>* vec) {
   cout << "\nType your number: ";
   int input {0};
   cin >> input;
 
-  vector<int> temp_vec {};
-  IntToVector(&temp_vec, input);
+  vector<int> temp_vec = IntToVector(input);
 
   for(int i: temp_vec) {
-    long long unsigned int temp = static_cast<long long unsigned int>(OctTranslate(i));
-    vector<int> temp_vec2 {};  
-    IntToVector(&temp_vec2, temp);
-    if(temp < 100) {
-      if(temp/10 >= 1) {
-        temp_vec2.insert(temp_vec2.begin(), 0);
-      } else {
-        for(int i {0}; i < 2; i++) {temp_vec2.insert(temp_vec2.begin(), 0);}
-      }
-    }
-    for(int i: temp_vec2) {(*vec).push_back(i);}
+    vector<int> temp = OctTranslate(i);
+    for(int i: temp) {(*vec).push_back(i);}
   }
 }
 
@@ -131,80 +133,70 @@ void HexToBinary(vector<int> *vec) {
   cin >> input;
 
   for(char i: input) {
-    long long unsigned int temp = static_cast<long long unsigned int>(HexTranslate(i));
-    vector<int> temp_vec {};  
-    IntToVector(&temp_vec, temp);
-    if(temp < 1000) {
-      if(temp/100 >= 1) {
-        temp_vec.insert(temp_vec.begin(), 0);
-      } else if(temp/10 >= 1) {
-        for(int i {0}; i < 2; i++) {temp_vec.insert(temp_vec.begin(), 0);}
-      } else {
-        for(int i {0}; i < 3; i++) {temp_vec.insert(temp_vec.begin(), 0);}
-      }
-    }
-    for(int i: temp_vec) {(*vec).push_back(i);}
+    vector<int> temp = HexTranslate(i);
+
+    for(int i: temp) {(*vec).push_back(i);}
   }
 }
 
-unsigned int BinaryToDecimal(vector<int> *vec) {
-  unsigned int temp {0};
-  for(int i {static_cast<int>((*vec).size())-1}; i >= 0; i--) {
-    if((*vec).at((*vec).size()-i-1) == 1) {
-      temp += pow(2, i);
-    }
-  }
-  return temp;
-}
 
-unsigned int BinaryToDecimal(vector<int> *vec, long long unsigned int x) {
-  IntToVector(vec, x);
-  unsigned int temp {0};
-  for(int i {static_cast<int>((*vec).size())-1}; i >= 0; i--) {
-    if((*vec).at((*vec).size()-i-1) == 1) {
-      temp += pow(2, i);
-    }
-  }
-  return temp;
-}
+
+
 
 int main() {
   vector<int> vec {}; 
   vector<int> *vec_ptr = &vec;
 
-  unsigned int choice {0};
-  cout << "1. Decimal\n2. Oct\n3. Binary\n4. Hex\n";
-  cout << "What is your input going to be: ";
-  cin >> choice;
-
-  unsigned int decimal {0};
   
-  switch(choice) {
-    case 1: {
-      cout << "\nType you number: ";
-      cin >> decimal;
-      DecimalToBinary(vec_ptr, decimal);
-      break;
-    }
-    case 2: {
-      OctToBinary(vec_ptr);
-      decimal = BinaryToDecimal(vec_ptr); 
-      break;
+
+  size_t decimal {0};
+
+  size_t choice {0};
+  cout << "1. Decimal\n2. Oct\n3. Binary\n4. Hex\n";
+  while (true){
+    
+    cout << "\nWhat is your input going to be: ";
+    cin >> choice;
+
+    switch(choice) {
+      case 1: {
+        cout << "\nType you number: ";
+        cin >> decimal;
+        cout << endl;
+        DecimalToBinary(vec_ptr, decimal);
+        break;
       }
-    case 3: {
-      long long unsigned int x {0};
-      cout << "\nType the binary number: ";
-      cin >> x;
-      decimal = BinaryToDecimal(vec_ptr, x); 
-      break;
+      case 2: {
+        OctToBinary(vec_ptr);
+        decimal = BinaryToDecimal(vec_ptr); 
+        break;
+        }
+      case 3: {
+        long long unsigned int x {0};
+        cout << "\nType the binary number: ";
+        cin >> x;
+        vec = IntToVector(x);
+        decimal = BinaryToDecimal(vec_ptr); 
+        break;
+        }
+      case 4: {
+        HexToBinary(vec_ptr);
+        decimal = BinaryToDecimal(vec_ptr); 
+        break;
+      default:
+        continue;
       }
-    default: {
-      HexToBinary(vec_ptr);
-      decimal = BinaryToDecimal(vec_ptr); 
     }
+    break;
   }
 
   cout << "\nDecimal: " << decimal;
+
+  if(vec.at(0) == 0) {
+    while(vec.at(0) == 0) {
+      vec.erase(vec.begin());
+    }
+  }
 
   cout << "\nBinario: ";
   for(int i: vec) {
